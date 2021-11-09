3d6a4eff711ff05c226e10ce3914efae31cfc8c9
Nikolay Potapov <potapov4444gmail.ru>
2021-11-09 22:10:24 +0700

    add avtotest

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,11 +3,12 @@ package ru.netology.sqr;
 public class SQRService {
 
     public int calculateSqrt(int bot, int top) {
+        int counter = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= bot && i * i <= top) {
-                return i;
+                counter = counter + 1;
             }
         }
-        return 0;
+        return counter;
     }
 }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,11 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @Test
-    void shoutSqrt() {
+    void amountOfSquares() {
         SQRService sqrService = new SQRService();
-        int expected = 10;
-        int actual = sqrService.calculateSqrt(100, 300);
+        int expected = 4;
+        int actual = sqrService.calculateSqrt(200, 300);
         assertEquals(expected, actual);
 
     }
+
+    @Test
+    void borderValues() {
+        SQRService sqrService = new SQRService();
+        int expected = 91;
+        int actual = sqrService.calculateSqrt(100, 9801);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void valuesToTheBorder() {
+        SQRService sqrService = new SQRService();
+        int expected = 89;
+        int actual = sqrService.calculateSqrt(99, 9800);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void valuesOutOfBorder() {
+        SQRService sqrService = new SQRService();
+        int expected = 89;
+        int actual = sqrService.calculateSqrt(101, 9802);
+        assertEquals(expected, actual);
+    }
+
 }
